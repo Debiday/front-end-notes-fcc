e63c56f5aab0d986be6eb323b10b520f105fdff4
Deborah <Deborah.oyt@gmail.com>
2021-07-10 22:01:58 -0700

    Added 1 practice

--- js/intermediatealgos.js
+++ js/intermediatealgos.js
@@ -406,5 +406,89 @@ console.log(truthCheck([{"single": ""}, {"single": "double"}], "single"));
 
 
 // ---------------------------------------------------
+// TODO: Adding and optional arguments
+// ---------------------------------------------------
+function addTogether() {
+  // Function to check if a number is actually a number
+  function checkNum(num) {
+    return (typeof num === "number");
+  };
+
+  if (arguments.length === 2) {
+    // Check if we have two arguments and if they are numbers
+    // Return the sum if they are both numbers
+    let first = arguments[0];
+    let second = arguments[1];
+    if (checkNum(first) && checkNum(second)) {
+      return first + second;
+    } else {
+      return undefined;
+    }
+  } else if (arguments.length === 1) {
+    // If only one argument was found, return a new function
+    let first = arguments[0];
+    if (checkNum(first)) {
+      // Return function that expect a second argument.
+      function addSecond(second) {
+        // Check if the new argument is a number
+        if (checkNum(second)) {
+          return first + second;
+        } else {
+          return undefined;
+        }
+      };
+      return addSecond;
+    } else {
+      return undefined;
+    }
+  } else {
+    // Incorrect number of arguments found
+    return undefined;
+  }
+}
+
+// test here
+addTogether(2, 3);
+
+
+// ---------------------------------------------------
+// TODO: Make a person?
+// ---------------------------------------------------
+var Person = function(firstAndLast) {
+  // Only change code below this line
+  // Complete the method below and implement the others similarly
+  this.getFullName = function() {
+    return firstAndLast;
+  };
+  
+  this.getLastName = function() {
+    return firstAndLast.split(" ")[1];
+  };
+
+  this.getFirstName = function() {
+    return firstAndLast.split(" ")[0];
+  };
+
+  this.setFirstName = function(first) {
+    firstAndLast = first + " " + firstAndLast.split(" ")[1];
+  };
+
+   this.setLastName = function(last) {
+    firstAndLast = firstAndLast.split(" ")[0] + " " + last;
+  };
+
+  this.setFullName = function(fullName) {
+    firstAndLast = fullName;
+  }
+  return firstAndLast;
+};
+
+var bob = new Person('Bob Ross');
+bob.setFullName("Haskell Curry");
+console.log(bob.getFullName());
+console.log(bob.getFirstName());
+
+
+// ---------------------------------------------------
 // TODO: 
 // ---------------------------------------------------
