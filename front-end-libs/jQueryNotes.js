008b5118eff5e4367bfe52ec36ea87d8f96a7183
Deborah Ong <deborah.oyt@gmail.com>
2021-07-13 18:45:23 -0700

    Add sass notes

--- front-end-libs/jQueryNotes.js
+++ front-end-libs/jQueryNotes.js
@@ -4,6 +4,28 @@
 
 // Jquery
 // Some available functions
+<script>
+  $(document).ready(function() {
+    $("#target1").css("color", "red");
+    $("#target1").prop("disabled", true);
+    $("#target4").remove();
+    $("#target2").appendTo("#right-well");
+    //function chaining
+    $("#target5").clone().appendTo("#left-well")
+  });
+</script>
+
+
+//Others
+$("#target1").parent().css("background-color", "red");
+$("#right-well").children().css("color", "orange");
+$(".target:nth-child(2)").addClass("animated bounce");
+$(".target:even").addClass("animated shake");
+
+//hinge
+$("body").addClass("animated hinge");
+
+
 
 
 
