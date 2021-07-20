44a21d10958bda570a642dca99126e107bf0b2b5
Deborah Ong <deborah.oyt@gmail.com>
2021-07-19 18:05:12 -0700

    Add react notes

--- react/functions2.js
+++ react/functions2.js
@@ -349,3 +349,28 @@ class myComponent extends React.Component {
             )
         }}
     };
+/* --------------------------------------------------- */
+/* Render with && condition (more concise) */
+/* --------------------------------------------------- */
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            display: true
+        }
+        this.toggleDisplay = this.toggleDisplay.bind(this);
+    }
+    toggleDisplay() {
+        this.setState(state => ({
+            display: !state.display
+        }));
+    }
+    render() {
+        return(
+            <div>
+                <button onClick={this.toggleDisplay}></button>
+                {this.state.display && <h1>Displayed!</h1>}
+            </div>
+        );
+    }
+ };
