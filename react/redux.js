0a006a7e0c85b1ac70a0c08542a639a643f472b3
Deborah Ong <deborah.oyt@gmail.com>
2021-07-21 09:56:42 -0700

    Add redux notes

--- react/redux.js
+++ react/redux.js
@@ -27,13 +27,44 @@ const action = {
     return action;
   }
 /* --------------------------------------------------- */
-/* */
+/* Dispatch an action event */
 /* --------------------------------------------------- */
+const store = Redux.createStore(
+    (state = {login: false}) => state
+  );
+  
+  const loginAction = () => {
+    return {
+      type: 'LOGIN'
+    }
+  };
+  
+  // Dispatch the action here:
+  store.dispatch(loginAction());
+/* --------------------------------------------------- */
+/* Handle an action in the store */
+/* --------------------------------------------------- */
+const defaultState = {
+    login: false
+};
 
-/* --------------------------------------------------- */
-/* */
-/* --------------------------------------------------- */
+const reducer = (state = defaultState, action) => {
+    if (action.type === "LOGIN") {
+        return {
+            login: true
+        };
+    } else {
+        return state
+    };
+};
 
+const store = Redux.createStore(reducer);
+
+const loginAction = () => {
+    return {
+        type: 'LOGIN'
+    }
+};
 /* --------------------------------------------------- */
 /* */
 /* --------------------------------------------------- */
