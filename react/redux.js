22b64e4f424e666bd5844ac998e898c5678adbc3
Deborah Ong <deborah.oyt@gmail.com>
2021-07-21 23:00:58 -0700

    Add redux notes

--- react/redux.js
+++ react/redux.js
@@ -66,8 +66,40 @@ const loginAction = () => {
     }
 };
 /* --------------------------------------------------- */
-/* */
+/* Using switch statements to handle multiple actions */
 /* --------------------------------------------------- */
+const defaultState = {
+    authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+    switch(action.type) {
+        case "LOGIN":
+            return {
+                authenticated: true
+            };
+        case "LOGOUT":
+            return {
+                authenticated: false
+            };
+        default:
+            return defaultState;
+    }
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+    return {
+        type: 'LOGIN'
+    }
+};
+
+const logoutUser = () => {
+    return {
+        type: 'LOGOUT'
+    }
+};
 /* --------------------------------------------------- */
 /* */
 /* --------------------------------------------------- */
