8fe61b04ba5c8f76f92d53b6c1e208a67c4648fe
Deborah Ong <deborah.oyt@gmail.com>
2021-07-29 10:48:21 -0700

    Add redux notes

--- react/redux.js
+++ react/redux.js
@@ -399,3 +399,27 @@ const store = Redux.createStore(immutableReducer);
 /* --------------------------------------------------- */
 /* Copy an object with Object.assign()*/
 /* --------------------------------------------------- */
+const defaultState = {
+    user: 'CamperBot',
+    status: 'offline',
+    friends: '732, 982',
+    community: 'freeCodeCamp'
+};
+
+const immutableReducer = (state = defaultState, action) => {
+    switch(action.type) {
+        case 'ONLINE':
+            const newObject = Object.assign({}, state, {status: 'online'})
+            return newObject
+        default:
+            return state;
+    }
+};
+
+const wakeUp = () => {
+    return {
+        type: 'ONLINE'
+    }
+};
+
+const store = Redux.createStore(immutableReducer);
